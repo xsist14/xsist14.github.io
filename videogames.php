6c886b0367f07416d3a673fc086e30edd9224519
xsist14 <jason.d.guest@gmail.com>
2019-11-06 20:15:17 -0800

    Added new section and silenced some errors

--- videogames.php
+++ videogames.php
@@ -1,6 +1,3 @@
-<?php
-	$currentgame = "Dragon Quest XI"; 
-?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -10,9 +7,10 @@
 	<body>
 		<nav id="nav">
 			<li><a href="index.html">Home</a></li>
+			<li><a href="animation.html">Animation</a></li>
 		</nav>
 		<p>
-			Hello, welcome to the video game page. The game I'm currently playing is <?php echo $currentgame ?>
+			Hello, welcome to the video game page. The game I'm currently playing is Dragon Quest XI!
 		</p>
 	</body>
 </html>
